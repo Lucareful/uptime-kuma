ea05101098f2cde5914d83104ec5d1c92880e6b4
Lucareful <34530552+Lucareful@users.noreply.github.com>
2024-12-24 11:57:00 +0800

    Create uptime.config.ts

---
+++ uptime.config.ts
@@ -0,0 +1,8 @@
+const pageConfig = {
+  title: "Luenci Status Page",
+  links: [
+    { link: 'https://github.com/Lucareful', label: 'GitHub' },
+    { link: 'https://luenci.com/', label: 'Blog' },
+    { link: 'mailto:lucarful@gmail.com', label: 'Email Me', highlight: true },
+  ],
+}
